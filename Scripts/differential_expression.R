ec4ecdf0ce3263f73789df5e0d34dd40ae41726a
dbdanso <dansob.daniel@gmail.com>
2026-08-23 17:15:11 +0000

    Implement run_limma function for DE analysis
    Added a function for differential expression analysis using limma.

---
+++ Scripts/differential_expression.R
@@ -0,0 +1,30 @@
+# --------------------------------------------------
+# Script: Differential Expression Analysis (limma-voom)
+# Description: Performs differential gene expression analysis across bacterial infection datasets using limma and edgeR
+# ----------------------------------------------------
+# Load required libraries
+library(limma)
+library(edgeR)
+# Define condition vector
+condition <- factor(c("control", "control", "control", "infected", "infected", "infected"))
+#Design matrix
+design <- model.matrix(~ condition)
+#Function for DE analysis
+run_limma <- function (count_matrix, output_name) {
+  #Create DGEList
+  dge <- DGEList(counts)
+  # Normalize counts
+  dge <- calcNormFactors(dge)
+  # Voom transformation
+  v <- voom(dge, design, plot=TRUE)
+  #Linear modeling
+  fit <- lmFit(v, design)
+  fit <- eBayes(fit)
+  # Extract results
+  results <- topTable(fit, coef=2, number=Inf)
+  # Filter significant DEGs
+  deg <- results_AV[results$adj.P.Val < 0.05 &abs(results_AV$logFC) > 1,]
+  #Save results
+  write.csv(deg, paste0("DEG_results_", output_name,"_limma.csv"))
+}
+
