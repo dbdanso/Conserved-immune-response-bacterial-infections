c6a00bf3a9d0a53f1444cf53b6895c18ea13789c
dbdanso <dansob.daniel@gmail.com>
2026-08-24 14:35:02 +0000

    Update 4.5_Gene_Cnet.R

--- Scripts/04_visualization/4.5_Gene_Cnet.R
+++ Scripts/04_visualization/4.5_Gene_Cnet.R
@@ -1,1 +1,9 @@
-
+# Load libraries
+library(clusterProfiler)
+library(org.Hs.eg.db) 
+library(AnnotationDbi)
+library(enrichplot)
+library(ggplot2)
+library(dplyr)
+# Run Helper function 3 (Gene enrichment)
+res <- 
